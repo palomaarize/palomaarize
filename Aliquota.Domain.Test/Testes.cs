76cb8d796efbc1c98b758dc7ded2aacf30ccf0ac
palomaarize <62774605+palomaarize@users.noreply.github.com>
2020-03-29 14:43:26 -0300

    primeiro commit

---
+++ Aliquota.Domain.Test/Testes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Calculo;
+
+namespace Aliquota.Domain.Test
+{
+    public class Testes
+    {
+        [Fact]
+        public void Validacao()
+        {
+
+            Login user = new Login()
+            {
+                Nome = "paloma",
+                Senha = 1234,
+            };
+
+            Assert.Contains("paloma", user.Nome);
+            Assert.Equal(1234, user.Senha);
+
+
+
+
+            //if ((user.Nome == "paloma") & (user.Senha == 1234))
+            //{
+            //    var validado = true;
+            //}
+
+
+
+
+        }
+
+    }
+}
---
+++ Calculo/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Calculo
+{
+    public class Login
+    {
+        public string NickName;
+        public string Senha;
+ 
+        public Login(string nickname, string senha)
+        {
+            Senha = senha;
+            NickName = nickname;
+
+            nickname = String.Empty;
+            if (!string.IsNullOrEmpty(nickname))
+            {
+
+            }
+
+        }
+
+    }
+
+        
+   
+}
